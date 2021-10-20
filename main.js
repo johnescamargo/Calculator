c87cf0fad4ed04b0f2987173ff0dea42762d0a7f
Johnes Camargo <38084663+johnescamargo@users.noreply.github.com>
2021-10-20 17:11:03 -0300

    Calculator, first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+//Display result of calculations
+displayRes = (resultCal) => {
+    document.getElementById("result").innerHTML = resultCal; 
+}
+
+//Clean textarea tag
+acFunc = () => {
+    document.getElementById("result").innerHTML = "";
+}
+
+//Clean textarea tag
+plusMinusFunc = () => {
+    let num = document.getElementById("result");
+}
+
+//Display result of calculations
+percentageFunc = (num, numPerc) => {
+    //some code here 
+}
+
+//Display number 0
+func0 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display number 1
+func1 = () => {
+    document.getElementById("result").innerHTML += 1;
+}
+//Display  number 2
+func2 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display  number 3
+func3 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+func4 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+func5 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+func6 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+func7 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+func8 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+func9 = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+funcComma = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+funcEqual = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+funcPlus = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+funcMinus = (num, numPerc) => {
+    //some code here 
+}
+
+//Display result of calculations
+funcDivision = (num, numPerc) => {
+    //some code here 
+}
